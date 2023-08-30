1deb62ca72c47d10e04b3c82d9bb861a09bd70a3
Yuliana Montaño <yulianamarcela200@gmail.com>
2023-08-30 11:52:51 -0400

    Configuracion de las variables de entorno

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,37 @@ import { PokemonModule } from './pokemon/pokemon.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
+import { ConfigModule } from '@nestjs/config';
+import { EnvConfigurations } from './config/app.config';
+import { JoiValidationSchema } from './config/joi.validation';
 
 @Module({
   imports: [
+
+    ConfigModule.forRoot({
+      load: [EnvConfigurations],
+      validationSchema: JoiValidationSchema
+    }),
 
     ServeStaticModule.forRoot({
       rootPath: join(__dirname,'..','public'),
       }),
 
-    MongooseModule.forRoot('mongodb://localhost/nestjs-pokemon'),
+    MongooseModule.forRoot(process.env.MONGO_DB),
+
 
     PokemonModule,
 
     CommonModule,
 
-    SeedModule,
+    SeedModule
+
+
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(){
+  }
+}
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -5,14 +5,22 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { PaginationDto } from '../common/dto/pagination.dto';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
-  ) { }
+    private readonly configService: ConfigService,
+  ) {
+
+    this.defaultLimit = this.configService.get<number>('defaultLimit');
+
+   }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
@@ -29,7 +37,7 @@ export class PokemonService {
   }
 
   findAll(paginationDto: PaginationDto) {
-    const { limit=10, offset=0 } = paginationDto;
+    const { limit= this.defaultLimit, offset=0 } = paginationDto;
     return this.pokemonModel.find().skip(offset).limit(limit).sort({ no: 1 }).select('-__v')
   }
 
